bad0b3c81086bf7eeb6319fe6979001fa49d5d74
岡村 昌樹 <m-okamura@sd00665-mm.local>
2013-09-02 18:57:56 +0900

    OSのディストリビューションを選択する関数を修正
    MacOSとLinuxを判別しさらにLinuxディストリビューションを判定するロジックを追加

--- distro.sh
+++ distro.sh
@@ -1,15 +1,43 @@
-#! /bin/bash
+#! /bin/bash 
 
 function package_installer() {
-	cmd=""
-	if [ $(which aptitude) != "" ]; then
-		cmd=$(which aptitude)
-	elif [ $(which yum) != "" ]; then
-		cmd=$(which yum)
-	elif [ $(which brew) != "" ]; then
-		cmd=$(which brew)
-	fi
+	case $(choose_ostype) in
+		darwin*)
+			cmd=$(which brew)
+		;;
+		redhat*)
+			cmd=$(which yum)
+		;;
+		ubuntu*)
+			cmd=$(which aptitude)
+		;;
+	esac
 	echo ${cmd}
 	return 0
 }
 
+function choose_ostype() {
+	case ${OSTYPE} in
+		darwin*)
+			# Mac向け設定
+			echo "darwin"
+		;;
+		linux*)
+			# 更にDistoributionを判定する
+			choose_linux
+		;;
+	esac
+	return 0
+}
+
+function choose_linux() {
+	case $(lsb_release -i --short) in
+		CentOS | Scientific)
+			echo "redhat"
+		;;
+		LinuxMint)
+			echo "ubuntu"
+		;;
+	esac
+	return 0
+}
